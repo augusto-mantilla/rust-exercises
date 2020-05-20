b881af22a4c34ccdc0e4584b09ef88922ad2777d
Augusto <aug.ornelas@gmail.com>
2020-05-20 18:47:45 +0100

    Add test for the ownership exercise

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,12 +1,75 @@
-// Create a function that returns the length of a string without
-// taking ownership of the value
+// Complete the signature and the body of the `str_len` function so it
+// receives a string or a string literal and returns its length (of
+// type usize)
+// without taking ownership of the value
 
 fn main() {
-	let s = String::from("Hello");
-	let l = str_len(&s);
-	println!("\tlen(\"{}\") = {}", s, l);
+	let s = "hello";
+	let s1 = "hello".to_string();
+
+	println!("\tstr_len(\"{}\") = {}", s, str_len(s));
+	println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
 }
 
-fn str_len(s: &str) -> usize {
+fn str_len() {
 	// Write you're code here
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	// maybe not the best way to make the test, but I wanted to use
+	// lifetimes
+	fn it_works() {
+		struct TstLit<'a> {
+			str: &'a str,
+			l: usize,
+		}
+
+		struct TstString {
+			str: String,
+			l: usize,
+		}
+
+		let tsts = vec![
+			TstLit { str: "hello", l: 5 },
+			TstLit { str: "how", l: 3 },
+			TstLit {
+				str: "are you",
+				l: 7,
+			},
+			TstLit {
+				str: "change",
+				l: 6,
+			},
+		];
+		let o_tsts = vec![
+			TstString {
+				str: "hello".to_string(),
+				l: 5,
+			},
+			TstString {
+				str: "how".to_string(),
+				l: 5,
+			},
+			TstString {
+				str: "are you".to_string(),
+				l: 5,
+			},
+			TstString {
+				str: "change".to_string(),
+				l: 5,
+			},
+		];
+
+		for t in tsts.iter() {
+			assert_eq!(t.l, str_len(t.str));
+		}
+
+		for t in o_tsts.iter() {
+			assert_eq!(t.l, str_len(&t.str));
+		}
+	}
 }
