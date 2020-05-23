f66a9eea9463451013e9fabcae1a43dc4476e5c8
Augusto <aug.ornelas@gmail.com>
2020-05-23 20:25:58 +0100

    Puts the borrow exercise in its own directory

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,24 +1,20 @@
-// Complete the signature and the body of the `str_len` function so it
-// receives a string or a string literal and returns its length (of
-// type usize)
-// without taking ownership of the value
-
-fn main() {
-	let s = "hello";
-	let s1 = "hello".to_string();
-
-	println!("\tstr_len(\"{}\") = {}", s, str_len(s));
-	println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
-}
-
-fn str_len() {
-	// Write you're code here
-}
-
-// Modify the function first_subword so that takes ownership of a string and returns the
+// Create a function that takes ownership of a string and returns the
 // first sub-word in it. It should work for camelCase as well as snake_case
 // first_subword(camelCase) returns camel
 // first_subword(snake_case) returns snake
+
+// And fix the printing expression so the code works
+fn main() {
+	let s1 = String::from("helloWorld");
+	let s2 = String::from("snake_case");
+	let s3 = String::from("CamelCase");
+	let s4 = String::from("just");
+
+	println!("first_subword({}) = {}", s1, first_subword(s1)); // Must print first_subword(helloWorld) = hello
+	println!("first_subword({}) = {}", s2, first_subword(s2)); // Must print first_subword(snake_case) = snake
+	println!("first_subword({}) = {}", s3, first_subword(s3)); // Must print first_subword(CamelCase) = Camel
+	println!("first_subword({}) = {}", s4, first_subword(s4)); // Must print first_subword(just) = just
+}
 
 fn first_subword(s: ) -> String {
 }
@@ -26,60 +22,6 @@ fn first_subword(s: ) -> String {
 #[cfg(test)]
 mod tests {
 	use super::*;
-
-	#[test]
-	// maybe not the best way to make the test, but I wanted to use
-	// lifetimes
-	fn str_len_test() {
-		struct TstLit<'a> {
-			str: &'a str,
-			l: usize,
-		}
-
-		struct TstString {
-			str: String,
-			l: usize,
-		}
-
-		let tsts = vec![
-			TstLit { str: "hello", l: 5 },
-			TstLit { str: "how", l: 3 },
-			TstLit {
-				str: "are you",
-				l: 7,
-			},
-			TstLit {
-				str: "change",
-				l: 6,
-			},
-		];
-		let o_tsts = vec![
-			TstString {
-				str: "hello".to_string(),
-				l: 5,
-			},
-			TstString {
-				str: "how".to_string(),
-				l: 3,
-			},
-			TstString {
-				str: "are you".to_string(),
-				l: 7,
-			},
-			TstString {
-				str: "change".to_string(),
-				l: 6,
-			},
-		];
-
-		for t in tsts.iter() {
-			assert_eq!(t.l, str_len(t.str));
-		}
-
-		for t in o_tsts.iter() {
-			assert_eq!(t.l, str_len(&t.str));
-		}
-	}
 
 	#[test]
 	fn first_subword_test() {
