f7144de1913ac2007577e729c2a0d593d18b3e5b
Augusto <aug.ornelas@gmail.com>
2020-05-29 17:58:28 +0100

    Add an exercise to exemplify the use of tuples

---
+++ tuples/src/main.rs
@@ -0,0 +1,17 @@
+// Define a tuple to represent a student
+// Each student is identified by an id number of type i32
+// Its name and its last name as a string
+// Print the tuple to stdout
+
+// Note: &str is a string slice in Rust for this exercise you just need
+// to know that string literals like "hello" are string slices
+fn main() {
+	let student: (i32, &str, &str) = /* id, first_name, last_name */
+	println!("Student: {}", );
+	// Print only the first name
+	println!("Student first name: {}",);
+	// Print only the last name
+	println!("Student last name: {}",);
+	// Print only the id
+	println!("Student Id: {}",);
+}
