c56b6e01666db45df239834f533785938864cf30
Augusto <aug.ornelas@gmail.com>
2020-05-30 19:53:40 +0100

    Add an exercise to practice mutable borrow

---
+++ mutability/src/main.rs
@@ -0,0 +1,19 @@
+// Write a function called doubtful that add to every passed to it a ?
+
+// You have to fix the code to make it compile an for that you can
+// only modify the code where is shown
+
+fn main() {
+	//You can add code in the next line before `s`
+	s = String::from("Hello");
+	println!("Before changing the string: {}", s);
+	change_str(/*you can add code here*/);
+	println!("After changing the string: {}", s);
+}
+
+// This function doesn't return anything
+fn change_str(/*you can add code here*/) {
+	//Add the body of the function here
+}
+
+// Borrow mutably a String an add, remove or modify the content
