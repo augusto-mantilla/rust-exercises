7aae17f43d7eedec550af3f595d9afa80fcfea34
Augusto <aug.ornelas@gmail.com>
2020-07-02 22:23:51 +0100

    Add exercises for generics

---
+++ generics/src/main.rs
@@ -0,0 +1,44 @@
+// Write a functions called identity that calculates the identity of a
+// value (receives any data type and returns the same value)
+
+fn main() {
+	println!("Hello, world!");
+	println!("{}", identity(3));
+	// Should print 3
+}
+
+fn identity(v: ) ->  {
+	
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[derive(PartialEq, Debug)]
+	struct Point {
+		x: i32,
+		y: i32,
+	}
+
+	#[test]
+	fn test_with_int() {
+		assert_eq!(identity(3), 3);
+	}
+
+	#[test]
+	fn test_with_float() {
+		assert_eq!(identity(1.0), 1.0);
+	}
+
+	#[test]
+	fn test_with_str() {
+		assert_eq!(identity("you"), "you");
+	}
+
+	#[test]
+	fn test_with_struct() {
+		let s = Point { x: 1, y: 2 };
+		assert_eq!(identity(&s), &s);
+	}
+}
