75bc190a0a36a998f39e6903c25b8378c9f9766c
Augusto <aug.ornelas@gmail.com>
2020-08-04 12:04:35 +0100

    Add test and fix some issues in the `arrays` exercise

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -1,27 +1,42 @@
 // Define a function call thirtytwo_tens that returns an array with 32
-// positions fill with only the value 10:
-// [10, 10, 10, ... 10].len() = 32
+// positions fill with only the value 10: [10, 10, 10, ... 10].len()
+// = 32
 
+// Write a function that takes an array of i32 and returns the sum of
+// the elements (make it work with the main)
 fn main() {
 	let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	let a1: Vec<i32> = (1..11).collect();
 	let b = [5; 10];
 
 	println!("The Sum of the elements in {:?} = {}", a, sum(a));
-	println!("The Sum of the elements in {:?} = {}", a, sum_v2(a));
 	println!("The Sum of the elements in {:?} = ", a1);
 	println!("The Sum of the elements in {:?} = {}", b, sum(b));
 	println!(
-		"Array size 50 with all elements to 10 = {:?}",
-		thirtytwo_tens().len()
+		"Array size {} with only 10's in it {:?}",
+		thirtytwo_tens().len(),
+		thirtytwo_tens()
 	);
 }
 
-fn thirtytwo_tens() ->  {
+fn sum(a: [i32])  {
 }
 
-// Complete the function sum to return the sum of the all the elements
-// in the vector passed as an argument
-fn sum() -> i32 {
+fn thirtytwo_tens() -> [] {
 }
 
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn test_thirtytwo_tens() {
+		assert_eq!(thirtytwo_tens(), [10; 32]);
+	}
+
+	#[test]
+	fn test_sum() {
+		let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		assert_eq!(sum(a), a.iter().sum());
+	}
+}
