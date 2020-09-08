c6c5f7be8fbab16b0f4e30edb5f35a70db1e348e
Augusto <aug.ornelas@gmail.com>
2020-09-08 17:32:51 +0100

    Use the correct precision for the comparison of floating points

--- temperature_conv/src/main.rs
+++ temperature_conv/src/main.rs
@@ -2,8 +2,8 @@
 // celsius_to_fahrenheit to
 // Make this code compile and pass the test
 fn main() {
-	println!("{} F = {} C", -459.67, celsius_to_fahrenheit(-459.67));
-	println!("{} C = {} F", 0.0, fahrenheit_to_celsius(0.0));
+	println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+	println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
 }
 
 fn fahrenheit_to_celsius(f: f64) -> f64 {
@@ -17,15 +17,16 @@ fn celsius_to_fahrenheit(c: f64) -> f64 {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use std::f64::EPSILON;
 
 	fn eql(a: f64, b: f64) -> bool {
-		(b - a).abs() > 0.00000001
+		(b - a).abs() < EPSILON
 	}
 
 	#[test]
 	fn test_f_to_c() {
-		assert!(eql(fahrenheit_to_celsius(20.0), -6.666666667));
-		assert!(eql(fahrenheit_to_celsius(83.0), 28.333333333));
+		assert!(eql(fahrenheit_to_celsius(20.0), -6.666666666666666));
+		assert!(eql(fahrenheit_to_celsius(83.0), 28.333333333333332));
 	}
 
 	#[test]
