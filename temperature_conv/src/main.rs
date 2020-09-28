ded3bff422eca4bfaa3528964eb28c9b52675735
Augusto <aug.ornelas@gmail.com>
2020-09-28 18:55:55 +0100

    Adds new exercise `blood_types`

--- temperature_conv/src/main.rs
+++ temperature_conv/src/main.rs
@@ -2,8 +2,8 @@
 // celsius_to_fahrenheit to
 // Make this code compile and pass the test
 fn main() {
-	println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
-	println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
+	println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67)); // It should print -459.67 F = -459.67 F = -273.15 C
+	println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0)); // It should print 0.0 C = 32 F
 }
 
 fn fahrenheit_to_celsius(f: f64) -> f64 {
